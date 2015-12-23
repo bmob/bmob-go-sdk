d1cb069d84a43f52a66ce08be05b40620032d54d
Kassian <kassiansun@gmail.com>
2015-12-23 09:09:21 +0800

    update ImageReq struct

--- image.go
+++ image.go
@@ -1,25 +1,25 @@
 package bmob
 
 //https://api.bmob.cn/1/images/thumbnail
+type ImageReq struct {
+	Image   string `json:"image"`
+	OutType int    `json:"outType"`
+}
 type ThumbnailReq struct {
-	Image string `json:"image"`
-
+	ImageReq
 	Mode    string `json:"mode"`
 	Quality string `json:"quality"`
 	Width   string `json:"width"`
-
-	OutType int `json:"outType"`
 }
 
 //https://api.bmob.cn/1/images/watermark
 type WatermarkReq struct {
-	Image     string `json:"image"`
+	ImageReq
 	Watermark string `json:"watermark"`
 	Dissolve  string `json:"dissolve"`
 	DistanceX string `json:"distanceX"`
 	DistanceY string `json:"distanceY"`
 	Gravity   string `json:"gravity"`
-	OutType   int    `json:"outType"`
 }
 
 type ImageResponse struct {
